3a611a95ce6dc388a8c5acf1282c24506098cafa
Ferens Anastasiia <nastyaferens2.0@gmail.com>
2025-03-24 17:26:58 +0200

    update js

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 const sortByDescendingFriendCount = (users) =>
-    users.toSorted((a, b) => b.friends.lenght - a.friends.lenght);
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 console.log(
   sortByDescendingFriendCount([
     {
--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,5 @@
-const getTotalBalanceByGender = (users, gender)
+const getTotalBalanceByGender = (users, gender) => 
+  users
     .filter(user => user.gender === gender)
     .reduce((total, user) => total + user.balance, 0);
     const clients = [
